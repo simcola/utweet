572562affd7e2f27cfeaab161f656edec59c0336
Simeon Lifschitz <simcola@hotmail.com>
2026-01-25 16:55:20 -0500

    Add tsconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  // Your other config here if any...
+  webpack(config) {
+    config.resolve.alias['@'] = __dirname;  // Maps @/ to project root
+    // Alternative more explicit version:
+    // config.resolve.alias['@/*'] = path.resolve(__dirname, './*');
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
 
